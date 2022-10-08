68f40251addc5a048601cae999833e693d0b9964
ErikB <80177106+ErikBahena@users.noreply.github.com>
2022-10-07 18:10:14 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const daysNode = document.getElementById('days');
+const hoursNode = document.getElementById('hours');
+const minutesNode = document.getElementById('minutes');
+const secondsNode = document.getElementById('seconds');
+
+const randomTime = () => {
+  const days = Math.floor(Math.random() * 30);
+  const hours = Math.floor(Math.random() * 12);
+  const minutes = Math.floor(Math.random() * 60);
+  const seconds = Math.floor(Math.random() * 60);
+
+  return { days, hours, minutes, seconds };
+};
+
+const main = async () => {
+  const { days, hours, minutes, seconds } = randomTime();
+
+  daysNode.textContent = days;
+  hoursNode.textContent = hours;
+  minutesNode.textContent = minutes;
+  secondsNode.textContent = seconds;
+
+  // starts a timer that will run every second and update the UI
+  const timer = setInterval(() => {
+    if (
+      secondsNode.textContent === '0' &&
+      minutesNode.textContent === '0' &&
+      hoursNode.textContent === '0' &&
+      daysNode.textContent === '0'
+    ) {
+      clearInterval(timer);
+      return;
+    }
+
+    secondsNode.textContent--;
+
+    if (secondsNode.textContent < 0) {
+      secondsNode.textContent = 59;
+      minutesNode.textContent--;
+    }
+
+    if (minutesNode.textContent < 0) {
+      minutesNode.textContent = 59;
+      hoursNode.textContent--;
+    }
+
+    if (hoursNode.textContent < 0) {
+      hoursNode.textContent = 23;
+      daysNode.textContent--;
+    }
+  }, 1000);
+};
+
+main();
